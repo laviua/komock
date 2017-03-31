1dd63a95e67a8ca3f72aa6001a68e4370fe2c615
Oleksandr <oleksandrloushkin@MacBook-Pro-Oleksandr.local>
2017-03-31 14:07:50 +0300

    Add timeout configuration for callbacks. Remove body from callback request. build script refactoring Coroutine instead of async client

--- komock-core/src/main/kotlin/ua/com/lavi/komock/engine/RequestHandlerBuilder.kt
+++ komock-core/src/main/kotlin/ua/com/lavi/komock/engine/RequestHandlerBuilder.kt
@@ -1,9 +1,10 @@
 package ua.com.lavi.komock.engine
 
 
-import org.apache.http.HttpResponse
-import org.apache.http.concurrent.FutureCallback
-import org.apache.http.impl.nio.client.HttpAsyncClients
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.launch
+import org.apache.http.client.config.RequestConfig
+import org.apache.http.impl.client.HttpClients
 import org.slf4j.LoggerFactory
 import ua.com.lavi.komock.engine.handler.AfterRequestHandler
 import ua.com.lavi.komock.engine.handler.BeforeRequestHandler
@@ -13,7 +14,6 @@ import ua.com.lavi.komock.engine.model.Request
 import ua.com.lavi.komock.engine.model.Response
 import ua.com.lavi.komock.engine.model.config.http.AnyRequest
 import ua.com.lavi.komock.engine.model.config.http.RouteProperties
-import java.lang.Exception
 import java.util.regex.Pattern
 
 
@@ -104,31 +104,27 @@ class RequestHandlerBuilder(val routeProperties: RouteProperties) {
         val requestHandler = object : CallbackHandler {
             override fun handle(request: Request, response: Response) {
                 if (routeProperties.callback.enabled) {
-                    val callbackProperties = routeProperties.callback
 
-                    val httpclient = HttpAsyncClients.createDefault()
-                    httpclient.use { httpclient ->
-                        httpclient.start()
-                        val request = AnyRequest(callbackProperties.httpMethod, callbackProperties.url)
-                        callbackProperties.requestHeaders.forEach { header -> request.addHeader(header.key, header.value) }
-                        httpclient.execute(request, object : FutureCallback<HttpResponse> {
-                            override fun failed(ex: Exception?) {
-                                log.info("Callback request is failed. ${ex!!.message}")
-                            }
+                    //callback request will be invoked in the another thread
+                    launch(CommonPool) {
+                        val callbackProperties = routeProperties.callback
 
-                            override fun completed(result: HttpResponse?) {
-                                log.info("Callback request is successfully completed")
-                            }
+                        val httpclient = HttpClients.createMinimal()
+                        val anyRequest = AnyRequest(callbackProperties.httpMethod, callbackProperties.url)
+                        val requestConfig = RequestConfig.custom()
+                                .setConnectTimeout(callbackProperties.connectTimeout)
+                                .setConnectionRequestTimeout(callbackProperties.connectionRequestTimeout)
+                                .setSocketTimeout(callbackProperties.socketTimeout)
+                                .build()
 
-                            override fun cancelled() {
-                                log.info("Callback request is cancelled")
-                            }
-
-                        })
-                        log.info("Callback sent to: ${callbackProperties.url}")
+                        callbackProperties.requestHeaders.forEach { header -> anyRequest.addHeader(header.key, header.value) }
+                        anyRequest.config = requestConfig
+                        try {
+                            log.info(httpclient.execute(anyRequest).statusLine.toString())
+                        } catch (t: Throwable) {
+                            log.warn(t.message)
+                        }
                     }
-
-
                 }
             }
         }
--- komock-core/src/main/kotlin/ua/com/lavi/komock/engine/model/config/http/CallbackProperties.kt
+++ komock-core/src/main/kotlin/ua/com/lavi/komock/engine/model/config/http/CallbackProperties.kt
@@ -7,6 +7,8 @@ class CallbackProperties {
     var enabled: Boolean = false
     var httpMethod: String = ""
     var url: String = ""
-    var requestBody: String = ""
+    var connectTimeout: Int = 250000
+    var connectionRequestTimeout: Int = 30000
+    var socketTimeout: Int = 25000
     var requestHeaders:Map<String,String> = HashMap()
 }
